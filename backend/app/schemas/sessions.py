c667da4d61cacb0189b305af0b2eeed411b93f8d
TobiMohr <t-mohr@online.de>
2025-09-25 13:49:06 +0200

    add: backend for weight

--- backend/app/schemas/sessions.py
+++ backend/app/schemas/sessions.py
@@ -3,15 +3,19 @@ from datetime import datetime
 from typing import List, Optional
 from .criterias import CriterionRead
 
+class CriterionWithWeight(BaseModel):
+    id: int
+    weight: int = 1
+
 class SessionBase(BaseModel):
     title: str
     description: Optional[str] = None
 
 class SessionCreate(SessionBase):
-    criteria: Optional[List[int]] = []
+    criteria: Optional[List[CriterionWithWeight]] = []
 
 class SessionUpdate(SessionBase):
-    criteria: Optional[List[int]] = []
+    criteria: Optional[List[CriterionWithWeight]] = []
 
 class SessionRead(SessionBase):
     id: int
