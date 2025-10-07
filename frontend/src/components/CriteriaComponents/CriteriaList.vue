1e7a7b7e5e98c8eaaf303e253f51638d5cb8d542
TobiMohr <t-mohr@online.de>
2025-10-07 14:44:52 +0200

    refactor: small grammar error

--- frontend/src/components/CriteriaComponents/CriteriaList.vue
+++ frontend/src/components/CriteriaComponents/CriteriaList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="max-w-7xl mx-auto mt-8 bg-white p-6 rounded-xl shadow-md">
     <div class="flex justify-between items-center mb-4">
-      <h2 class="text-2xl font-bold text-gray-800">Criterias</h2>
+      <h2 class="text-2xl font-bold text-gray-800">Criteria</h2>
       <router-link to="/criterias/create">
         <BaseButton class="bg-green-500 hover:bg-green-600">
           Create Criterion
