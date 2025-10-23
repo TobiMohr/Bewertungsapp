0c145c31095551576a2dad0173ee1f74733d3e08
Tobias Mohr <to411moh@htwg-konstanz.de>
2025-10-23 09:52:21 +0200

    added: teams to backend

--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -14,6 +14,7 @@ from sqlalchemy.orm import relationship
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
+    team_id = Column(Integer, ForeignKey("teams.id"), nullable=True)
     first_name = Column(String, index=True)
     last_name = Column(String, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
@@ -29,7 +30,22 @@ class User(Base):
     )
 
     # Relationships
+    team = relationship("Team", back_populates="users")
     criteria = relationship("UserCriterion", back_populates="user")
+
+# --- Team table ---
+class Team(Base):
+    __tablename__ = "teams"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, nullable=False)
+    created_at = Column(
+        String,
+        default=lambda: datetime.now(timezone.utc).isoformat()
+    )
+
+    # Relationships
+    users = relationship("User", back_populates="team")
 
 
 # --- Association table between User and Criterion ---
--- backend/app/schemas/users.py
+++ backend/app/schemas/users.py
@@ -1,21 +1,26 @@
 # schemas.py
 from pydantic import BaseModel, EmailStr
+from typing import Optional
+from .teams import TeamRead
 
 class UserCreate(BaseModel):
     first_name: str
     last_name: str
+    team_id: Optional[int] = None
     email: EmailStr
     password: str
 
 class UserUpdate(BaseModel):
     first_name: str
     last_name:str
+    team_id: Optional[int] = None
     email: EmailStr
 
 class UserRead(BaseModel):
     id: int
     first_name: str
     last_name: str
+    team_id: Optional[TeamRead] = None
     email: EmailStr
 
     class Config:
