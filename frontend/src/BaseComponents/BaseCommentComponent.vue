9b2864d8508cf1c4e40570462d0d6f3b567b4d98
Tobias Mohr <to411moh@htwg-konstanz.de>
2025-10-27 15:07:04 +0100

    add: frontend for comments

---
+++ frontend/src/BaseComponents/BaseCommentComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="p-3 border rounded-lg shadow-sm">
+    <p class="text-gray-900 font-medium">{{ comment.text }}</p>
+    <p class="text-gray-400 text-sm mt-1">Created: {{ formatDate(comment.created_at) }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseCommentComponent",
+  props: {
+    comment: { type: Object, required: true },
+  },
+  methods: {
+    formatDate(dt) {
+      return new Date(dt).toLocaleString();
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
